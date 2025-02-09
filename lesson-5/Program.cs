ae8188e82c224c2a1f7a4f68cd9a5b63eff7465e
pavel <sailofgeaven@gmail.com>
2025-02-09 20:01:39 +0300

    feat: add work with tasks and refactoring

--- lesson-5/Program.cs
+++ lesson-5/Program.cs
@@ -1,22 +1,67 @@
-﻿
+
+using System.Text;
+
 namespace lesson_5;
 
 class Program
 {
     static string? userName = null;
+    static List<string> tasks = new List<string>();
+
+    static void Main(string[] args)
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+        Console.InputEncoding = Encoding.GetEncoding("utf-16"); //почемуто в bash не принимает нормально кририлицу
+
+        Greetings();
+        string? command = "";
+        while (command != "/exit")
+        {
+            command = Console.ReadLine();
+            ExecuteCommand(command);
+        }
+    }
+
+    private static void ExecuteCommand(string? command)
+    {
+        switch (command)
+        {
+            case "/start":
+                StartCommand();
+                break;
+            case "/info":
+                InfoCommand(); break;
+            case "/help":
+                HelpCommand(); break;
+            case "/addtask":
+                AddTaskCommand(); break;
+            case "/removetask":
+                RemoveTaskCommand(); break;
+            case "/showtasks":
+                ShowTasksCommand(); break;
+            case string parts when parts.StartsWith("/echo") && userName != null:
+                EchoCommand(parts.Substring("/echo ".Length)); break;
+            case "/exit": return;
+            default:
+                Console.WriteLine("Недоступная команда. Используйте '/help' для списка доступных команд");
+                break;
+
+        }
+    }
 
     static void Greetings()
     {
-        Console.WriteLine("Добро пожаловать! Список доступных команд: /start, /help, /info, /exit.");
+        Console.WriteLine("Привет, мир!");
+        Console.WriteLine("Добро пожаловать! Доступные команды: /start, /help, /info, /echo, /addtask, /showtasks, /removetask, /exit");
         Console.WriteLine("Введите команду:");
     }
 
     static void StartCommand()
     {
-        Console.WriteLine("Введите имя:");
+        Console.Write("Введите имя: ");
         userName = Console.ReadLine();
 
-        if (userName?.Length > 0 )
+        if (!string.IsNullOrEmpty(userName))
         {
             Console.WriteLine($"{userName} теперь вам доступна команда /echo");
         }
@@ -41,40 +86,57 @@ class Program
         Console.WriteLine("Информация о приложении:");
         Console.WriteLine("Версия 0.1.0");
     }
-    static void Main(string[] args)
-    {
-        Greetings();
-        string? command = "";
-        while (command != "/exit")
-        {
-            command = Console.ReadLine();
-            ExecuteCommand(command);
-        }
-    }
 
     static void EchoCommand(string echo)
     {
         Console.WriteLine($"{userName}: {echo}");
     }
 
-    private static void ExecuteCommand(string? command)
+    static void AddTaskCommand()
     {
-        switch (command)
-        {
-            case "/start":
-                StartCommand();
-                break;
-            case "/info":
-                InfoCommand(); break;
-            case "/help":
-                HelpCommand(); break;
-            case string parts when parts.StartsWith("/echo") && userName != null:
-                EchoCommand(parts.Substring("/echo ".Length)); break;
-            case "/exit": return;
-            default:
-                Console.WriteLine("Недоступная команда. Используйте '/help' для списка доступных команд");
-                break;
+        Console.Write("Пожалуйста, введите описание задачи: ");
 
+        var task = Console.ReadLine();
+        if (!string.IsNullOrEmpty(task))
+        {
+            tasks.Add(task);
+            Console.WriteLine($"Задача \"{task}\" добавлена.");
+        }
+
+
+    }
+
+    static void RemoveTaskCommand()
+    {
+        Console.WriteLine("Вот ваш список задач:");
+        ShowTasks();
+        Console.Write("Введите номер задачи для удаления: ");
+
+        var isNumber = int.TryParse(Console.ReadLine(), out int index);
+
+        if (isNumber)
+        {
+            var task = tasks[index - 1];
+
+            tasks.RemoveAt(index - 1);
+            Console.WriteLine($"Задача \"{task}\" удалена.");
+        }
+
+
+    }
+
+    static void ShowTasks()
+    {
+        for (int i = 0; i < tasks.Count; i++)
+        {
+            Console.WriteLine($"{i + 1}. {tasks[i]}");
         }
     }
+
+    static void ShowTasksCommand()
+    {
+        ShowTasks();
+
+    }
+
 }
